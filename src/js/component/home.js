446b12eb83282c42955a6892649e35935ac1d386
fabianchs <fabichasola@hotmail.com>
2021-07-19 02:23:22 +0000

    working on labels

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,13 +1,30 @@
 import { func } from "prop-types";
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import { Input } from "reactstrap";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-	const [credits, setCredits] = useState([]);
-	const [grades, setGrades] = useState([]);
+	const [credits, setCredits] = useState([""]);
+	const [grades, setGrades] = useState([""]);
+	const [renderedEditor, setRenderedEditor] = useState([""]);
+
+	function editGrade(e, index) {
+		let aux_grades = grades;
+
+		aux_grades[index] = e.target.value;
+
+		setGrades(aux_grades);
+	}
+
+	function editCredit(e, index) {
+		let aux_credits = credits;
+
+		aux_credits[index] = e.target.value;
+
+		setGrades(aux_credits);
+	}
 
 	function addToArrays() {
 		let aux_credits = credits;
@@ -18,28 +35,70 @@ export function Home() {
 
 		setCredits(aux_credits);
 		setGrades(aux_grades);
+
+		createInputs();
 	}
 
 	function deleteFromArrays() {
 		let aux_credits = credits;
 		let aux_grades = grades;
 
-		aux_credits.pop();
-		aux_grades.pop();
+		if (aux_credits.length > 1) {
+			aux_credits.splice(-1, 1);
+			aux_grades.splice(-1, 1);
+		}
 
 		setCredits(aux_credits);
 		setGrades(aux_grades);
+
+		createInputs();
 	}
 
 	function createInputs() {
-		//hello
+		const list_inputs = credits.map((element, index) => (
+			<div className="row" key={index}>
+				<div className="col-6" key={index.toString + "credits"}>
+					{" "}
+					<Input
+						type="text"
+						name="text"
+						defaultValue={element}
+						placeholder="Credits"
+						className="m-1 border-dark"
+						onBlur={() => {
+							editCredit(event, index);
+						}}
+					/>
+				</div>
+				<div className="col-6" key={index.toString + "grades"}>
+					{" "}
+					<Input
+						type="text"
+						name="text"
+						placeholder="Grade"
+						className="m-1 border-dark"
+						defaultValue={grades[index]}
+						onBlur={() => {
+							editGrade(event, index);
+						}}
+					/>
+				</div>
+			</div>
+		));
+
+		setRenderedEditor(list_inputs);
 	}
+
+	useEffect(() => {
+		createInputs();
+	}, []);
+
 	return (
 		<div className="text-center text-light mt-5">
 			<h1>Hello! Here is a simple way to calculate the semester grade</h1>
 			<div className="row d-flex justify-content-center">
-				<div className="col-xl-6 col-lg-6 col-md-9 col-sm-12">
-					<div>
+				<div className="col-xl-3 col-lg-3 col-md-9 col-sm-12 ">
+					<div className="d-flex justify-content-center">
 						<button
 							type="button"
 							className="btn btn-light m-1"
@@ -57,12 +116,15 @@ export function Home() {
 							<i className="fas fa-minus"></i>
 						</button>
 					</div>
+					<div className="row">
+						<div className="col-6">Credits</div>
+						<div className="col-6">Grade</div>
+					</div>
+					<div key={[grades, credits]}>{renderedEditor}</div>
 				</div>
 			</div>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-info">
+
+			<a href="#" className="btn btn-info m-2">
 				Calculate
 			</a>
 		</div>
